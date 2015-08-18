e751b748a79d7ffe925d42ee6fa4fcadf0ae4d3d
Anil Bas <abas@ebay.com>
2015-08-18 09:43:15 +0300

    LinkedBlockingQueue Tests Completed

--- src/test/java/util/collection/test/QueueTest.java
+++ src/test/java/util/collection/test/QueueTest.java
@@ -6,14 +6,16 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
 public class QueueTest {
 
     @Test
-    public void baseQueueTest()
-    {
+    public void baseQueueTest() {
         Queue<Integer> queue = new PriorityQueue<Integer>();
 
         queue.add(10);
@@ -23,11 +25,11 @@ public class QueueTest {
 
         queue.remove();
 
-        assertEquals(8,(int)queue.element());
+        assertEquals(8, (int) queue.element());
     }
 
     @Test
-    public void peekQueueTest(){
+    public void peekQueueTest() {
         Queue<Integer> queue = new PriorityQueue<Integer>();
 
         queue.add(10);
@@ -35,13 +37,13 @@ public class QueueTest {
         queue.add(12);
         queue.add(90);
 
-        assertEquals(8,(int)queue.peek());
-        assertEquals(8,(int)queue.element());
+        assertEquals(8, (int) queue.peek());
+        assertEquals(8, (int) queue.element());
 
     }
 
     @Test
-    public void pollQueueTest(){
+    public void pollQueueTest() {
         Queue<Integer> queue = new PriorityQueue<Integer>();
 
         queue.add(10);
@@ -49,12 +51,12 @@ public class QueueTest {
         queue.add(12);
         queue.add(90);
 
-        assertEquals(8,(int)queue.poll());
-        assertEquals(10,(int)queue.element());
+        assertEquals(8, (int) queue.poll());
+        assertEquals(10, (int) queue.element());
     }
 
     @Test
-    public void offerQueueTest(){
+    public void offerQueueTest() {
         Queue<Integer> queue = new PriorityQueue<Integer>();
 
         queue.add(10);
@@ -62,13 +64,13 @@ public class QueueTest {
         queue.add(12);
         queue.add(90);
 
-        assertEquals(true,queue.offer(5));
-        assertEquals(5,(int)queue.peek());
+        assertEquals(true, queue.offer(5));
+        assertEquals(5, (int) queue.peek());
 
     }
 
     @Test
-    public void linkedListOfferTest(){
+    public void linkedListOfferTest() {
         Deque<Integer> queue = new LinkedList<Integer>();
 
         queue.add(10);
@@ -76,16 +78,16 @@ public class QueueTest {
         queue.add(12);
         queue.add(90);
 
-        assertEquals(10,(int)queue.peek());
+        assertEquals(10, (int) queue.peek());
         assertTrue(queue.offerFirst(5));
         assertTrue(queue.offerLast(20));
-        assertEquals(5,(int)queue.peek());
-        assertEquals(20,(int)queue.peekLast());
+        assertEquals(5, (int) queue.peek());
+        assertEquals(20, (int) queue.peekLast());
 
     }
 
     @Test
-    public void linkedListClearTest(){
+    public void linkedListClearTest() {
         Deque<Integer> queue = new LinkedList<Integer>();
 
         queue.add(10);
@@ -95,8 +97,65 @@ public class QueueTest {
 
         queue.clear();
 
-        assertEquals(0,queue.size());
+        assertEquals(0, queue.size());
     }
 
+    @Test
+    public void linkedBlockingQueueBaseTest() throws InterruptedException {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>();
 
+        queue.add(10);
+        queue.add(8);
+        queue.add(12);
+        queue.add(90);
+
+        assertEquals(10, (int) queue.take());
+        assertEquals(3, queue.size());
+        assertEquals(8, (int) queue.peek());
+    }
+
+    @Test
+    public void linkedBlockingQueuePutTest() throws InterruptedException {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>();
+
+        queue.add(10);
+        queue.add(8);
+        queue.add(12);
+        queue.add(90);
+
+        assertEquals(4, queue.size());
+        queue.put(5);
+        assertEquals(5, queue.size());
+
+    }
+
+    @Test
+    public void linkedBlockingQueuePollTimeoutTest() throws InterruptedException {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>();
+
+        queue.add(10);
+        queue.add(8);
+        queue.add(12);
+        queue.add(90);
+
+        assertEquals(4, queue.size());
+        assertEquals(10, (int) queue.poll(50, TimeUnit.SECONDS));
+        assertEquals(3, queue.size());
+        assertEquals(8, (int) queue.peek());
+
+    }
+
+    @Test
+    public void linkedBlockingQueueRemainingCapacityTest() {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>();
+
+        queue.add(10);
+        queue.add(8);
+        queue.add(12);
+        queue.add(90);
+
+        assertEquals(2147483643, queue.remainingCapacity());
+        queue.add(50);
+        assertEquals(2147483642,queue.remainingCapacity());
+    }
 }
